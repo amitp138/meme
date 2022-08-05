f9633e5325f62c5d7a9d399c99ff799bc08ecf5b
amitpandey <amitpande1008@gmail.com>
2022-08-05 09:49:40 +0530

    second commit

--- src/pages/MemeCard.js
+++ src/pages/MemeCard.js
@@ -2,9 +2,23 @@ import React from 'react'
 import "./MemeCard.css";
 const MemeCard = (props) => {
   return (
-    <div>
+    <div className='card'>
         <div className="image">
-              <img key={props.value.id} src={props.value.urls.small} alt="amit" />
+              <img className='memepht' key={props.value.id} src={props.value.urls.small} alt="amit" />
+            </div>
+            <div className="bottom">
+              <div className="like">
+                <img src="./like.png" alt="" />
+              </div>
+              <div className="dislike">
+                <img src="./dislike.png" alt="" />
+              </div>
+              <div className="chat">
+                <img src="./chat.png" alt="" />
+              </div>
+              <div className="share">
+                <img src="./share.png" alt="" />
+              </div>
             </div>
     </div>
   )
